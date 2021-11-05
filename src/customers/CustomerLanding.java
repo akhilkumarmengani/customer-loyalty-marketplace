99a3b900e4e09e50c60b01b168929500b7751e02
akhilkumarmengani@gmail.com <akhilkumarmengani@gmail.com>
2021-11-05 12:29:28 -0400

    added gitignore

--- src/customers/CustomerLanding.java
+++ src/customers/CustomerLanding.java
@@ -2,10 +2,12 @@ package customers;
 
 import util.DisplayOptions;
 
+
 public class CustomerLanding {
 	
-	public void takeInput()
+	public void takeInput() throws Exception
 	{
+		
 		while(true)
 		{
 			DisplayOptions.printOptions(DisplayOptions.options.get(DisplayOptions.CustomerLanding));
@@ -21,23 +23,71 @@ public class CustomerLanding {
 			{
 				case 1:
 				{
+					//CustomerLandingUtil.enrollInLoyaltyProgram();
+					DisplayOptions.printOptions(DisplayOptions.options.get(DisplayOptions.Enroll));
 					
+					while(true) {
+						option = DisplayOptions.getSc().nextInt();
+						if(!CustomerLandingUtil.validateInput(option,1,2)) {
+							System.out.println("Please Enter A Valid Input :|");
+						}
+						else {
+							if(option==1) {
+								String enrolledBrand = CustomerLandingUtil.enrollInLoyaltyProgram();
+								if(enrolledBrand!=null) {
+									System.out.println("You are Enrolled in "+enrolledBrand);
+								}
+								else {
+									System.out.println("You Are Already Enrolled In This Loyalty");
+								}
+							}
+							else {
+								break;
+							}
+						}
+					}
 				}
 				case 2:
 				{
-					
+					//CustomerLandingUtil.getRewardActivities();	
+					while(true) {
+						option = DisplayOptions.getSc().nextInt();
+						if(!CustomerLandingUtil.validateInput(option,1,4)) {
+							System.out.println("Please Enter A Valid Input :|");
+						}
+						else {
+							if(option==1) {
+								
+							}
+							else if(option==2) {
+								
+							}
+							else if(option==3) {
+								
+							}
+							else {
+								break;
+							}
+						}
+					}
 				}
 				case 3:
 				{
-					
+					//CustomerLandingUtil.viewWallet();
+					while(true) {
+						
+					}
 				}
 				case 4:
 				{
-					
+					//CustomerLandingUtil.redeemPoints();
+					while(true) {
+						
+					}
 				}
 				case 5:
 				{
-					
+					return;
 				}
 				
 			}

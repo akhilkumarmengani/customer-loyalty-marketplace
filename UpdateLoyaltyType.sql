c109390a77226e8e443ba62bf3b6f2144e9dfd65
pranavsv16 <90572698+pranavsv16@users.noreply.github.com>
2021-11-07 16:01:37 -0500

    Add files via upload

---
+++ UpdateLoyaltyType.sql
@@ -0,0 +1,13 @@
+create or replace TRIGGER UPDATELOYALTYTYPE 
+AFTER INSERT ON Tiered_Loyalty_Programs
+FOR EACH ROW
+DECLARE
+loyalty_val VARCHAR2(20) := 'NULL';
+BEGIN
+        SELECT loyalty_type into loyalty_val from Brands where brand_id = :new.Brand_Id;
+        
+        IF loyalty_val <> 'TIERED'  THEN
+        --INSERT INTO regular_loyalty_programs (BRAND_ID,LOYALTY_PROGRAM_ID) VALUES (:NEW.Brand_Id,:NEW.Loyalty_Program_Id);
+        Update Brands SET loyalty_type = 'TIERED' where brand_id = :NEW.brand_id;
+        END IF;
+END;
